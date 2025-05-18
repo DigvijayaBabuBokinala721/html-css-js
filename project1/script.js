9876819ea500c136334552bef4e8b774e5420850
Digvijaya Babu Bokinala <159063422+DigvijayaBabuBokinala721@users.noreply.github.com>
2025-05-18 23:27:11 +0530

    Add files via upload

---
+++ project1/script.js
@@ -0,0 +1,12 @@
+function fun1() {
+  alert("price: 73,000/-");
+}
+function fun2() {
+  alert("price: 64,000/-");
+}
+function fun3() {
+  alert("price: 87,000/-");
+}
+function fun4() {
+  alert("price: 94,000/-");
+}
